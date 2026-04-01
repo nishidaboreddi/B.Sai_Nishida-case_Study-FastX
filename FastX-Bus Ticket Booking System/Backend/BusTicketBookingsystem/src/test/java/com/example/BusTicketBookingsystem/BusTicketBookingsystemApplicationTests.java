0ebfb5d97dd22fc3a18782f282977dee49746dac
Nishida781 <sainishida0156.sse@saveetha.com>
2026-04-01 15:10:30 +0530

    testing folder

---
+++ FastX-Bus Ticket Booking System/Backend/BusTicketBookingsystem/src/test/java/com/example/BusTicketBookingsystem/BusTicketBookingsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BusTicketBookingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusTicketBookingsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
